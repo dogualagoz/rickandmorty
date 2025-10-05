c6b135f7159cbb6bcb0b1f5afc413bdfd4d5a3f5
dogu <alagozdogu@gmail.com>
2025-10-05 17:29:27 +0300

    App scaffold is completed. Bottom bar and topbar is completed. Routers are completed

---
+++ lib/app/router.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:go_router/go_router.dart';
+import 'package:rickandmorty/views/app_view.dart';
+import 'package:rickandmorty/views/characters_view/characters_view.dart';
+import 'package:rickandmorty/views/favorites_view/favorites_view.dart';
+import 'package:rickandmorty/views/locations_view/locations_view.dart';
+import 'package:rickandmorty/views/sections_view/sections_view.dart';
+
+final _routerKey = GlobalKey<NavigatorState>();
+
+class AppRoutes {
+  AppRoutes._();
+  static const String characters = '/';
+  static const String favorites = '/favorites';
+  static const String locations = '/locations';
+  static const String sections = '/sections';
+}
+
+final router = GoRouter(
+  navigatorKey: _routerKey,
+  initialLocation: AppRoutes.characters,
+  routes: [
+    StatefulShellRoute.indexedStack(
+      builder:
+          (context, state, navigationShell) =>
+              AppView(navigationShell: navigationShell),
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoutes.characters,
+              builder: (context, state) => const CharactersView(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoutes.favorites,
+              builder: (context, state) => const FavoritesView(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoutes.locations,
+              builder: (context, state) => const LocationsView(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoutes.sections,
+              builder: (context, state) => const SectionsView(),
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
